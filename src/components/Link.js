3ea98c9294ef685e4dc4d69c416c49645f6cb9fb
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2021-12-13 16:23:11 +0530

    this is to let other components  know that the url is changed

--- src/components/Link.js
+++ src/components/Link.js
@@ -4,6 +4,10 @@ const Link = ({ href, className, children }) => {
     const onClick = (event) => {
         event.preventDefault();
         window.history.pushState({}, '', href);
+
+        // this is to let other components  know that the url is changed
+        const navEve = new PopStateEvent('popState');
+        window.dispatchEvent(navEve);
     }
     return (
         <a href={href} onClick={onClick} className={className}>{children}</a>
