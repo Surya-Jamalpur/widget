0575d9d761112f7f28e88e9d4a9f8f0f2c31df82
Surya Jamalpuri <SJamalpuri@ad.prolifics.com>
2021-12-13 16:29:02 +0530

    able to listen the location url change

--- src/components/route.js
+++ src/components/route.js
@@ -1,6 +1,14 @@
+import { useEffect } from 'react';
 
 const windowPath = window.location.pathname;
+
+const onLocationChange = () => {
+    console.log('Hey.. the location changed');
+}
 const Route = ({ path, children }) => {
+    useEffect(() => {
+        window.addEventListener('popState', onLocationChange)
+    }, [])
     return windowPath === path ? children : null
 }
 
